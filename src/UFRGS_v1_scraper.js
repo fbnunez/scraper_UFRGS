3556e025bb6e88e80d1ac3073acacf1a4a010b9b
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-08-23 21:56:07 -0300

    scrapping and returning available admission data

---
+++ src/UFRGS_v1_scraper.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const date = new Date();
+
+const BASE_URL =
+  'https://www1.ufrgs.br/PortalEnsino/GraduacaoProcessoSeletivo/index.php/DivulgacaoDadosChamamento';
+
+// getting admission data
+// obj key is the admission code for body data
+async function getAdmissionType(anoSelecao = date.getFullYear()) {
+  const admissionObject = {};
+  admissionObject[anoSelecao] = {};
+  const response = await fetch(`${BASE_URL}/carregaConcursos`, {
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset: ISO-8859-1',
+    },
+    body: `anoSelecao=${anoSelecao}`,
+    method: 'POST',
+  });
+
+  let text = await response.text();
+  const $ = cheerio.load(text, { decodeEntities: false });
+  $('option').each((i, element) => {
+    if (i === 0) return;
+    const cheerioElement = $(element);
+    const tagValue = cheerioElement.attr('value').toString();
+    let textElement = cheerioElement.text().trim();
+    textElement = iconv.decode(Buffer.from(textElement), 'utf-8');
+    admissionObject[anoSelecao][tagValue] = textElement;
+  });
+  return admissionObject;
+}
+
+getAdmissionType();
